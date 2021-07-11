9119b3b312b8067b174e5b59820c7d1def6c1e40
Masahiro noma <macharo.0316@gmail.com>
2021-07-12 01:37:58 +0900

    ParameterをMainに持ってきた

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 mod models;
 
 fn main(){
-    let mover_group_model = models::mover_group_mod::MoverGroupModel::new(300);
+    const MOVER_NUM: i32 = 300;
+    const TIME_INTERVAL: i64 = 1;
+    const CAR_MAX_VELOCITY: f64 = 51.1;
+    const TRAIN_CAPACITY: f64 = 150.0;
+    const TRAIN_VELOCITY: f64 = 31.1;
+    const DAYS: usize = 50;
+    
+    let mover_group_model = models::mover_group_mod::MoverGroupModel::new(MOVER_NUM);
+
     let simuration_model = models::simulation_mod::SimulationModel{
         mover_group_model: mover_group_model,
+        time_interval: TIME_INTERVAL,
+        car_max_velocity: CAR_MAX_VELOCITY,
+        train_capacity: TRAIN_CAPACITY,
+        train_velocity: TRAIN_VELOCITY,
     };
 
-    let days: usize = 100;
-    simuration_model.run(days);
+    simuration_model.run(DAYS);
 }
--- src/models/simulation_mod.rs
+++ src/models/simulation_mod.rs
@@ -2,14 +2,13 @@ use crate::models::mover_group_mod;
 
 pub struct SimulationModel{
     pub mover_group_model: mover_group_mod::MoverGroupModel,
+    pub time_interval: i64,
+    pub car_max_velocity: f64,
+    pub train_capacity: f64,
+    pub train_velocity: f64,
 }
 
 impl SimulationModel{
-    const TIME_INTERVAL: i64 = 1;
-    const CAR_MAX_VELOCITY: f64 = 51.1;
-    const TRAIN_CAPACITY: f64 = 150.0;
-    const TRAIN_VELOCITY: f64 = 31.1;
-
     pub fn run(mut self, days: usize){
         for day in 0..days{ 
             println!("\nday: {}",day);
@@ -35,18 +34,18 @@ impl SimulationModel{
         &self, 
         mut car_mover_group: Vec<mover_group_mod::MoverModel>,
     ) -> Vec<mover_group_mod::MoverModel>{
-        car_mover_group[0].velocity = SimulationModel::CAR_MAX_VELOCITY;
+        car_mover_group[0].velocity = self.car_max_velocity;
         let mut time: i64 = 0;
         let route_length = car_mover_group[0].route.get_route_length();
 
         while car_mover_group.last().unwrap().arrival_time == std::i64::MAX {
-            time += SimulationModel::TIME_INTERVAL;
+            time += self.time_interval;
 
             for car_id in 0..car_mover_group.len(){
                 if car_mover_group[car_id].start_time <= time{
                     //locationは前の時刻のvelocityを用い，velocityは現在のlocationを用いる
                     let location: f64 
-                        = car_mover_group[car_id].location + car_mover_group[car_id].velocity / 3.6 * SimulationModel::TIME_INTERVAL as f64;
+                        = car_mover_group[car_id].location + car_mover_group[car_id].velocity / 3.6 * self.time_interval as f64;
                     car_mover_group[car_id].location = location;
 
                     let velocity: f64 = 
@@ -54,7 +53,7 @@ impl SimulationModel{
                             51.1
                         }else{
                             let traffic_dencity: f64 = 1000.0 / (car_mover_group[car_id - 1].location - car_mover_group[car_id].location);
-                            (SimulationModel::CAR_MAX_VELOCITY - 0.58647 * traffic_dencity).max(0.0).min(SimulationModel::CAR_MAX_VELOCITY)
+                            (self.car_max_velocity - 0.58647 * traffic_dencity).max(0.0).min(self.car_max_velocity)
                         };
                     car_mover_group[car_id].velocity = velocity;
 
@@ -79,15 +78,15 @@ impl SimulationModel{
         for current_mover_id in 0..train_mover_group.len(){
             passengers += train_mover_group[current_mover_id].ride_num;
 
-            if passengers >= SimulationModel::TRAIN_CAPACITY{
+            if passengers >= self.train_capacity{
                 //一車両に乗った客の到着時間は同じになる
                 let arrival_time = 
-                    train_mover_group[current_mover_id].start_time as f64 + route_length / SimulationModel::TRAIN_VELOCITY * 3.6;
+                    train_mover_group[current_mover_id].start_time as f64 + route_length / self.train_velocity * 3.6;
 
                 for id in first_passenger_id..=current_mover_id{
                     train_mover_group[id].arrival_time = arrival_time as i64;
                     train_mover_group[id].location = route_length;
-                    train_mover_group[id].velocity = SimulationModel::TRAIN_VELOCITY;
+                    train_mover_group[id].velocity = self.train_velocity;
                 }
                 passengers = 0.0;
                 first_passenger_id = current_mover_id + 1;
@@ -95,11 +94,11 @@ impl SimulationModel{
         }
         if passengers > 0.0{
             //一車両に乗った客の到着時間は同じになる
-            let arrival_time = train_mover_group.last().unwrap().start_time as f64 + route_length / SimulationModel::TRAIN_VELOCITY * 3.6;
+            let arrival_time = train_mover_group.last().unwrap().start_time as f64 + route_length / self.train_velocity * 3.6;
             for id in first_passenger_id..train_mover_group.len(){
                 train_mover_group[id].arrival_time = arrival_time as i64;
                 train_mover_group[id].location = route_length;
-                train_mover_group[id].velocity = SimulationModel::TRAIN_VELOCITY;
+                train_mover_group[id].velocity = self.train_velocity;
             }
         }
         return train_mover_group;
